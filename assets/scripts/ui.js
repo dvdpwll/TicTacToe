dcdedc8f1b3238ea2ba4eff444beb6524165ef2f
dvdpwll <dvdpwll456@gmail.com>
2016-07-22 16:08:35 -0400

    add code for ui.js
    added code for ui.js file.

---
+++ assets/scripts/ui.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const app = require('../app');
+
+const success = (data) => {
+  console.log(data);
+};
+
+const failure = (error) => {
+  console.error(error);
+};
+
+module.exports = {
+  success,
+  failure,
+};
