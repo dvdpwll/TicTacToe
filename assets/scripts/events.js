1cdea8a329e35feac66a7302fc3e52c67f53366d
dvdpwll <dvdpwll456@gmail.com>
2016-07-22 16:04:45 -0400

    add code for events.js
    added code for events.js file.

---
+++ assets/scripts/events.js
@@ -0,0 +1,21 @@
+'use strict';
+const api = require('./api');
+const ui = require('./ui');
+
+const getFormFields = require('../../../lib/get-form-fields');
+
+const onSignUp = function (event) {
+  let data = getFormFields(this);
+  event.preventDefault();
+  api.signUp(data)
+    .done(ui.success)
+    .fail(ui.failure);
+};
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp);
+};
+
+module.exports = {
+  addHandlers,
+};
