9e5e4cbefe443038fef330190870b6e8964b423c
dvdpwll <dvdpwll456@gmail.com>
2016-07-25 18:27:07 -0400

    add modal for new game and clear
    added output and modal for new game and clear.

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,18 +4,78 @@ const ui = require('./ui');
 
 const getFormFields = require('../../lib/get-form-fields');
 
-const onSignUp = function (event) {
-  console.log(event);
-  let data = getFormFields(this);
-  console.log(data);
-  event.preventDefault();
-  api.signUp(data)
-    .done(ui.success)
-    .fail(ui.failure);
+//user sign up
+const onSignUp = function () {
+  //console.log('sign up');
+  $('#sign-up-modal').modal('show');
+  $('#sign-up-submit').on('click',function(){
+    //console.log('submit pressed');
+    //get text fields
+    let email = $('#sign-up-email').val();
+    let password = $('#sign-up-password').val();
+    let confirmPassword = $('#sign-up-confirm-password').val();
+    console.log(email);
+    console.log(password);
+    console.log(confirmPassword);
+    //close modal
+    $('#sign-up-modal').modal('hide');
+  });
+};
+
+//user log in
+const onLogIn = function () {
+  //console.log('logged in');
+  $('#log-in-modal').modal('show');
+  $('#log-in-submit').on('click',function(){
+    //console.log('log in pressed');
+    //get text fields
+    let email = $('#log-in-email').val();
+    let password = $('#log-in-password').val();
+    console.log(email);
+    console.log(password);
+    //close modal
+    $('#log-in-modal').modal('hide');
+  });
+};
+
+//user changes password
+const onChangePassword = function () {
+  //console.log('logged in');
+  $('#change-password-modal').modal('show');
+  $('#change-password-submit').on('click',function(){
+    //console.log('change password pressed');
+    //get text fields
+    let oldPassword = $('#current-password').val();
+    let NewPassword = $('#new-password').val();
+    console.log(oldPassword);
+    console.log(NewPassword);
+    //close modal
+    $('#change-password-modal').modal('hide');
+  });
+};
+
+//user log out
+const onLogOut = function () {
+  console.log('logged out');
+};
+
+//user new game
+const onNewGame = function () {
+  console.log('New Game');
+};
+
+//user clear board
+const onClearBoard = function () {
+  console.log('Clear Board');
 };
 
 const addHandlers = () => {
-  $('#sign-up').on('submit', onSignUp);
+  $('#sign-up').on('click', onSignUp);
+  $('#log-in').on('click', onLogIn);
+  $('#log-out').on('click', onLogOut);
+  $('#change-password').on('click', onChangePassword);
+  $('#new-game').on('click', onNewGame);
+  $('#clear-board').on('click', onClearBoard);
 };
 
 module.exports = {
