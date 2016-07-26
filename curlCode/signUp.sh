6655ac755f357931de5099f80cdc420cb1ace3c6
dvdpwll <dvdpwll456@gmail.com>
2016-07-26 10:00:49 -0400

    add curlCode folder and signUp.sh
    added a folder to hold curl tests, added curl script for sign up.

---
+++ curlCode/signUp.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#curl "http://localhost:3000/sign-up" \
+
+
+# API="http://httpbin.org/post"
+# API="http://localhost:3000/sign-up"
+API="http://tic-tac-toe.wdibos.com/sign-up/"
+E='aaaa'
+P='bbbb'
+P_C='bbbb'
+
+curl ${API} \
+  --include \
+  --request POST \
+  --data-urlencode "credentials[email]=${E}" \
+  --data-urlencode "credentials[password]=${P}" \
+  --data-urlencode "credentials[password_confirmation]=${P_C}"
+
+
+# --header "Content-Type: application/x-www-form-urlencoded"
+
+# data output from curl doesn't have a trailing newline
+echo
