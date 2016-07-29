7fe41a2ff321d061d5e2c8e24c5f3af6831c0a7c
dvdpwll <dvdpwll456@gmail.com>
2016-07-29 09:00:11 -0400

    fix new api

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@
 const app = {
   //api: 'http://tic-tac-toe.wdibos.com/',
   //api: 'http://httpbin.org',
-  api: 'https://aqueous-atoll-85096.herokuapp.com',
+  api: 'https://aqueous-atoll-85096.herokuapp.com/',
 };
 
 module.exports = {
