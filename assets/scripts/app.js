34d25893b8397b264d9b59549116525640663725
dvdpwll <dvdpwll456@gmail.com>
2016-07-29 08:56:22 -0400

    new api address
    testing new api address for deployment.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const app = {
-  api: 'http://tic-tac-toe.wdibos.com/',
-  //api: 'http://httpbin.org'
+  //api: 'http://tic-tac-toe.wdibos.com/',
+  //api: 'http://httpbin.org',
+  api: 'https://aqueous-atoll-85096.herokuapp.com',
 };
 
 module.exports = {
